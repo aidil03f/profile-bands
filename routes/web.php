b60ae7cc25be2a15c86e81407d30b54be524e0aa
Aidil <aidil03f@gmail.com>
2021-06-19 21:44:42 +0800

    create form band

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Band\BandController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\{Route,Auth};
 
@@ -7,3 +9,12 @@ use Illuminate\Support\Facades\{Route,Auth};
 Auth::routes();
 
 Route::get('/', HomeController::class)->name('home');
+
+Route::middleware('auth')->group(function(){
+    Route::get('dasboard', DashboardController::class)->name('dashboard');
+
+    Route::prefix('bands')->group(function(){
+        Route::get('create',[BandController::class,'create'])->name('bands.create');
+        Route::post('create',[BandController::class,'store']);
+    });
+});
