541a483566fb31ae1d7fe90e4c0bc1a4a6df6e63
Aidil <aidil03f@gmail.com>
2021-06-21 19:37:31 +0800

    edit form lyric

--- routes/web.php
+++ routes/web.php
@@ -49,5 +49,9 @@ Route::middleware('auth')->group(function(){
         Route::post('create',[LyricController::class,'store']);
         Route::get('table',[LyricController::class,'table'])->name('lyrics.table');
         Route::get('data-table',[LyricController::class,'dataTable'])->name('lyrics.datatable');
+        Route::get('{lyric:slug}', [LyricController::class,'show'])->name('lyrics.show');
+        Route::get('{lyric:slug}/edit', [LyricController::class,'edit']);
+        Route::put('{lyric:slug}', [LyricController::class,'update']);
+       
     });
 });
